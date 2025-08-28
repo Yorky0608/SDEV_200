32d0c4a1482565ba225279af85683397bea47ad2
Yorky0608 <ca06leb@gmail.com>
2025-08-28 19:23:50 -0400

    test_files_modules

---
+++ modules/src/convert_hexa_to_deci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class convert_hexa_to_deci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String hexa = input.nextLine();
+
+        int deci = hex2dec(hexa);
+
+        System.out.print(deci);
+    }
+
+    public static int hex2dec(String hex) {
+        int decicaml_value = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            decicaml_value = i;
+        }
+        return decicaml_value;
+    }
+}
