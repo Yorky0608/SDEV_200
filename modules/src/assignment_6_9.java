d74b36e101a6cecf9207bfb70b90dd1772b950a2
Yorky0608 <ca06leb@gmail.com>
2025-09-01 11:24:40 -0400

    changed for loop
    for looped change to not have to add 1 to i for footToMeter

--- modules/src/assignment_6_9.java
+++ modules/src/assignment_6_9.java
@@ -1,12 +1,17 @@
 
 public class assignment_6_9 {
+    
     public static void main(String[] args) {
 
+        /** Outputs: Feet    Meters  |   Meters  Feet
+         * ------------------------------------------
+         */
 		System.out.println("Feet\tMeters\t|\tMeters\tFeet");
         System.out.println("---------------------------------------");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("%d\t%.3f\t|\t%.3f\t%.3f\n", i + 1, footToMeter(i + 1), i * 5.0 + 20.0, meterToFoot(i * 5.0 + 20.0));
+        /** For loop to print the conversion table */
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("%d\t%.3f\t|\t%.3f\t%.3f\n", i, footToMeter(i + 1), i * 5.0 + 20.0, meterToFoot(i * 5.0 + 20.0));
         }
         
     }
