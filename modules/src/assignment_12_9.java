4e39d1f43060d3e2502d0b649c7c3cf51abb90e8
Yorky0608 <ca06leb@gmail.com>
2025-09-10 19:35:11 -0400

    Create assignment_12_9.java

---
+++ modules/src/assignment_12_9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class assignment_12_9 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a binary number: ");
+		String binary = input.nextLine();
+
+        int decimal = bin2Dec(binary);
+		System.out.println("Decimal equivalent: " + decimal);
+		
+		input.close();
+	}
+
+	public static int bin2Dec(String binaryString) throws BinaryFormatException {
+		for (char c : binaryString.toCharArray()) {
+			if (c != '0' && c != '1') {
+				throw new BinaryFormatException("Not a binary number");
+			}
+		}
+		return Integer.parseInt(binaryString, 2);
+	}
+}
+
+class BinaryFormatException extends NumberFormatException {
+	public BinaryFormatException(String message) {
+		super(message);
+	}
+}
